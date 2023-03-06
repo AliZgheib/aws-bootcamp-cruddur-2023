97c970d3fcf2db5205cacadf50534310e6bfa163
AliZgheib <zgheibali@gmail.com>
2023-03-06 04:34:52 +0200

    Add span/tracer to home activity

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,16 @@
 from datetime import datetime, timedelta, timezone
+
+from opentelemetry import trace
+
+tracer = trace.get_tracer(__name__)
+
 class HomeActivities:
   def run():
     now = datetime.now(timezone.utc).astimezone()
+
+    span = trace.get_current_span()
+    span.set_attribute("app.now", now.isoformat())
+
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
@@ -41,4 +50,7 @@ class HomeActivities:
       'replies': []
     }
     ]
+
+    span.set_attribute("app.results_length", len(results))
+
     return results
