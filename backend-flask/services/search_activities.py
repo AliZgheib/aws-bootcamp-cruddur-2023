415e392bd0af2790ea789b67c4d45f42406c7558
Ali Zgheib <zgheibali@gmail.com>
2023-03-12 22:26:59 +0000

    Add HoneyComb custom instrumentation

--- backend-flask/services/search_activities.py
+++ backend-flask/services/search_activities.py
@@ -1,6 +1,14 @@
 from datetime import datetime, timedelta, timezone
+
+from opentelemetry import trace
+tracer = trace.get_tracer(__name__)
+
 class SearchActivities:
   def run(search_term):
+
+    span = trace.get_current_span()
+    span.set_attribute("app.search_term", search_term)
+
     model = {
       'errors': None,
       'data': None
